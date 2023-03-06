ba46b5ec6cef3249bf46cd9886c59ad5728a0653
jhonnyizidoro <jhonny-cwb@hotmail.com>
2023-03-06 12:27:11 -0300

    feat: navigation translation

--- components/Navigation/Navigation.tsx
+++ components/Navigation/Navigation.tsx
@@ -1,8 +1,12 @@
+import { useTranslation } from '@/hooks/translation'
+
 import { FC } from 'react'
 
 import { NextSvg, PreviousSvg } from '@/svg'
 
 import styles from './Navigation.module.scss'
+import enUs from './translations/en-US.json'
+import ptBr from './translations/pt-BR.json'
 
 import Player from '@/components/Player/Player'
 
@@ -20,26 +24,34 @@ const Navigation: FC<Props> = ({
   light,
   dark,
   section,
-}) => (
-  <nav className={styles.wrapper}>
-    <Player dark={dark} light={light} />
-    <div className={styles.buttons}>
-      <button
-        onClick={onPrevious}
-        className={`${styles.button} ${section === 1 && styles.buttonDisabled}`}
-        style={{ background: light, color: dark }}
-      >
-        <PreviousSvg fill={dark} /> Previous
-      </button>
-      <button
-        onClick={onNext}
-        className={`${styles.button} ${section === 5 && styles.buttonDisabled}`}
-        style={{ background: light, color: dark }}
-      >
-        Next <NextSvg fill={dark} />
-      </button>
-    </div>
-  </nav>
-)
+}) => {
+  const { t } = useTranslation({ ptBr, enUs })
+
+  return (
+    <nav className={styles.wrapper}>
+      <Player dark={dark} light={light} />
+      <div className={styles.buttons}>
+        <button
+          onClick={onPrevious}
+          disabled={section === 1}
+          className={styles.button}
+          style={{ background: light, color: dark }}
+          aria-label={`${t.previousButtonText} ${t.buttonLabelSuffix}`}
+        >
+          <PreviousSvg fill={dark} /> {t.previousButtonText}
+        </button>
+        <button
+          onClick={onNext}
+          disabled={section === 5}
+          className={styles.button}
+          style={{ background: light, color: dark }}
+          aria-label={`${t.nextButtonText} ${t.buttonLabelSuffix}`}
+        >
+          {t.nextButtonText} <NextSvg fill={dark} />
+        </button>
+      </div>
+    </nav>
+  )
+}
 
 export default Navigation
