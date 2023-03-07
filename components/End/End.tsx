cf4e19e6901f1542b9d298ba0d20bcd1fa91289f
jhonnyizidoro <jhonny-cwb@hotmail.com>
2023-03-06 23:13:02 -0300

    feat: translation for end

--- components/End/End.tsx
+++ components/End/End.tsx
@@ -1,3 +1,5 @@
+import { useTranslation } from '@/hooks/translation'
+
 import Link from 'next/link'
 import { FC } from 'react'
 
@@ -5,47 +7,51 @@ import { EnvelopeSvg, GitHubSvg, LinkedinSvg, PhoneSvg } from '@/svg'
 
 import styles from './End.module.scss'
 import background from './images/background.jpg'
+import enUs from './translations/en-US.json'
+import ptBr from './translations/pt-BR.json'
 
 import Background from '@/components/Background/Background'
 
-const End: FC = () => (
-  <>
-    <Background image={background} opacity={0.15} />
-    <div className={styles.content}>
-      <h1 className={styles.title}>{`That's all!`}</h1>
-      <h2 className={styles.subtitle}>
-        Thank you for knowing me a little more
-      </h2>
-      <div className={styles.text}>Like what you saw? Contact me!</div>
-      <div className={styles.buttons}>
-        <div className={styles.button}>
-          <Link href='mailto:jhonny-cwb@hotmail.com' className={styles.link}>
-            <EnvelopeSvg />
-          </Link>
-          <Link href='tel:+5541991791053' className={styles.link}>
-            <PhoneSvg />
-          </Link>
+const End: FC = () => {
+  const { t } = useTranslation({ enUs, ptBr })
+
+  return (
+    <>
+      <Background image={background} opacity={0.15} />
+      <div className={styles.content}>
+        <h1 className={styles.title}>{t.title}</h1>
+        <h2 className={styles.subtitle}>{t.subtitle}</h2>
+        <div className={styles.text}>{t.text}</div>
+        <div className={styles.buttons}>
+          <div className={styles.button}>
+            <Link href='mailto:jhonny-cwb@hotmail.com' className={styles.link}>
+              <EnvelopeSvg />
+            </Link>
+            <Link href='tel:+5541991791053' className={styles.link}>
+              <PhoneSvg />
+            </Link>
+          </div>
+          <div className={styles.button}>
+            <Link
+              href='https://www.linkedin.com/in/jhonnyizidoro/'
+              rel='noopener'
+              className={styles.link}
+            >
+              <LinkedinSvg />
+            </Link>
+            <Link
+              href='https://github.com/jhonnyizidoro'
+              rel='noopener'
+              className={styles.link}
+            >
+              <GitHubSvg />
+            </Link>
+          </div>
         </div>
-        <div className={styles.button}>
-          <Link
-            href='https://www.linkedin.com/in/jhonnyizidoro/'
-            rel='noopener'
-            className={styles.link}
-          >
-            <LinkedinSvg />
-          </Link>
-          <Link
-            href='https://github.com/jhonnyizidoro'
-            rel='noopener'
-            className={styles.link}
-          >
-            <GitHubSvg />
-          </Link>
-        </div>
+        <small className={styles.textSmall}>{t.phone}</small>
       </div>
-      <small className={styles.textSmall}>Phone: +55 41 9 9179-1053</small>
-    </div>
-  </>
-)
+    </>
+  )
+}
 
 export default End
